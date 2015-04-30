98a77177be699bcc4a9dd1671208609a0305af5e
Andrey Kuzmin <chipsoid@cpan.org>
2015-04-30 12:09:01 +0400

    First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Yandex::Metrika - It's new $module
+
+# SYNOPSIS
+
+    use Yandex::Metrika;
+
+# DESCRIPTION
+
+Yandex::Metrika is ...
+
+# LICENSE
+
+Copyright (C) Andrey Kuzmin.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Andrey Kuzmin <chipsoid@cpan.org>
---
+++ lib/Yandex/Metrika.pm
@@ -0,0 +1,40 @@
+package Yandex::Metrika;
+use 5.008001;
+use utf8;
+use Modern::Perl;
+
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Yandex::Metrika - It's new $module
+
+=head1 SYNOPSIS
+
+    use Yandex::Metrika;
+
+=head1 DESCRIPTION
+
+Yandex::Metrika is ...
+
+=head1 LICENSE
+
+Copyright (C) Andrey Kuzmin.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Andrey Kuzmin E<lt>chipsoid@cpan.orgE<gt>
+
+=cut
+
